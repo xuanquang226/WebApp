af89a956a986a668cadd0741af6889ab0e4ab30c
ngphuan2017 <phuan2017@gmail.com>
2023-09-26 16:06:35 +0700

    version 1.0.2

--- target/generated-sources/annotations/com/annp/pojo/OrderDetail_.java
+++ target/generated-sources/annotations/com/annp/pojo/OrderDetail_.java
@@ -8,7 +8,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-09-25T00:02:57", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-09-26T15:51:08", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(OrderDetail.class)
 public class OrderDetail_ { 
 
